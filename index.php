882d45b8392a446ddb2064e62bba2c59b5e058e1
anupam2704 <anupam2704@gmail.com>
2018-02-05 19:05:15 +0530

    Section Creation

--- index.php
+++ index.php
@@ -1,17 +1,58 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body >
+<?php include "front/header.php"; ?>
+
+<section class="section-added">
+    <div class="click-area">
+        <h4 class="text-top">
+            Click if you have turned your <br> Full Stop to Semicolon!
+        </h4>
+        <div class="semicolon-click">
+            <div class="semicolon-holder">
+                <span>;</span>
+            </div>
+        </div>
+        <div class="counter">
+            1000
+        </div>
+        <h6>
+            People have added more to life!
+        </h6>
+    </div>
+</section>
+<section class="section-story">
+    <h2 class="story-title">
+        Semicolon : The Story Behind
+    </h2>
+    <p>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+        <br>
+        blah
+    </p>
+</section>
+<section class="section-highlight">
+
+</section>
+</body>
+
 </html>
